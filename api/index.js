366f972505e5880cecef64b044ae2d2f3efa75b1
jfarroyo291 <j-a-m291@hotmail.com>
2022-08-11 12:14:59 -0500

    conn force false

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const { uploadType } = require('./src/controllers/typeController');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     await uploadType();
