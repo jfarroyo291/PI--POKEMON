7b7a3482e8cced98dacb350488db0598411fc767
jfarroyo291 <j-a-m291@hotmail.com>
2022-08-11 12:35:53 -0500

    modified index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from './axios';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store/index';
