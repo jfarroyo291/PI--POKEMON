8596c4f1ddceb33d2f619bd78f57c1971cdc75c8
jfarroyo291 <j-a-m291@hotmail.com>
2022-08-11 15:33:44 -0500

    conn force true without type preloading

--- api/index.js
+++ api/index.js
@@ -19,12 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { uploadType } = require('./src/controllers/typeController');
+//const { uploadType } = require('./src/controllers/typeController');
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, async () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-    await uploadType();
+    //await uploadType();
   });
 });
